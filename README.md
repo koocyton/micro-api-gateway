2820942b8c3b58c1d124edea3ed1274e98e8a8cf
yi.liu <liuyi@doopp.com>
2019-04-18 19:01:42 +0800

    files

--- README.md
+++ README.md
@@ -1,1 +1,104 @@
-# micro-api-gateway
+# reactor-guice
+
+Reactor-guice integrates the framework of Google Guice and Reactor-netty
+
+
+### 1. import reactor-guice
+
+#### maven
+```
+<dependency>
+    <groupId>com.doopp</groupId>
+    <artifactId>reactor-guice</artifactId>
+    <version>0.0.5</version>
+</dependency>
+```
+
+#### gradle
+```
+compile 'com.doopp:reactor-guice:0.0.5'
+```
+
+#### use Local Maven 
+```
+mvn clean
+
+mvn package
+
+mvn install:install-file -Dfile=target/reactor-guice-0.0.5.jar -DgroupId=com.doopp.local -DartifactId=reactor-guice -Dversion=0.0.5 -Dpackaging=jar
+
+<dependency>
+    <groupId>com.doopp.local</groupId>
+    <artifactId>reactor-guice</artifactId>
+    <version>0.0.5</version>
+</dependency>
+```
+
+### 2. create you application
+
+```java
+Injector injector = Guice.createInjector(...);
+
+KReactorServer.create()
+        .bind(host, port)
+        .injector(injector)
+        .handlePackages("you_handle_package")
+        // .addFilter("/", AppFilter.class)
+        .launch();
+```
+
+### 3. creat you service
+
+#### Handle Example
+
+```java
+/** https://kreactor.doopp.com/test/json **/
+@GET
+@Path("/json")
+@Produces({MediaType.APPLICATION_JSON})
+public Mono<Map<String, String>> json() {
+    return Mono
+        .just(new HashMap<String, String>())
+        .map(m -> {
+            m.put("hi", "five girl");
+            return m;
+        });
+}
+
+/** https://kreactor.doopp.com/test/jpeg **/
+@GET
+@Path("/jpeg")
+@Produces({"image/jpeg"})
+public Mono<ByteBuf> jpeg() {
+    return HttpClient.create()
+        .get()
+        .uri("https://static.cnbetacdn.com/article/2019/0402/6398390c491f650.jpg")
+        .responseContent()
+        .aggregate()
+        .map(ByteBuf::retain);
+}
+```
+
+
+#### WebSocket
+
+```java
+
+@Path("/kreactor/ws")
+@Singleton
+public class WsTestHandle extends AbstractWebSocketServerHandle {
+
+    @Override
+    public void connected(Channel channel) {
+        System.out.println(channel.id());
+        super.connected(channel);
+    }
+
+    @Override
+    public void onTextMessage(TextWebSocketFrame frame, Channel channel) {
+        System.out.println(frame.text());
+        super.onTextMessage(frame, channel);
+    }
+}
+
+```
---
+++ src/main/java/com/doopp/api/gateway/GatewayServer.java
@@ -0,0 +1,79 @@
+package com.doopp.api.gateway;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpMethod;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.http.server.HttpServer;
+
+public class GatewayServer {
+
+    public static void main(String[] args) {
+
+        HttpClient httpClient = HttpClient.create();
+
+        // 抛出线程监听 etcd ，并刷新 Api Gateway 的路由表
+        new Thread(() -> {
+
+        }).start();
+
+        // 启动 API Gateway Server
+        DisposableServer disposableServer = HttpServer.create()
+                .tcpConfiguration(tcpServer ->
+                        tcpServer.option(ChannelOption.SO_KEEPALIVE, true)
+                )
+                .handle((req, resp) -> {
+
+                    HttpClient httpClientRequest = httpClient
+                            .baseUrl("https://www.doopp.com")
+                            .headers(entries -> {
+                                HttpHeaders httpHeaders = req.requestHeaders();
+                                entries.set("User-Agent", httpHeaders.get("User-Agent"));
+                                // for(String name : httpHeaders.names()) {
+                                //    System.out.printf("%s: %s\n", name, httpHeaders.get(name));
+                                //    entries.set(name, httpHeaders.get(name));
+                                // }
+                            });
+
+                    HttpClient.ResponseReceiver<?> responseReceiver;
+
+                    // POST
+                    if (req.method().equals(HttpMethod.POST)) {
+                        responseReceiver = httpClientRequest.post()
+                                .send(req.receiveContent().ofType(ByteBuf.class));
+                    }
+                    // PUT
+                    else if (req.method().equals(HttpMethod.PUT)) {
+                        responseReceiver = httpClientRequest.put()
+                                .send(req.receiveContent().ofType(ByteBuf.class));
+                    }
+                    // DELETE
+                    else if (req.method().equals(HttpMethod.DELETE)) {
+                        responseReceiver = httpClientRequest.delete();
+                    }
+                    // GET
+                    else {
+                        responseReceiver = httpClientRequest.get().uri("/phpinfo.php");
+                    }
+
+                    return responseReceiver.response((httpClientResponse, byteBufFlux)->{
+                                resp.headers(httpClientResponse.responseHeaders());
+                                return byteBufFlux;
+                            })
+                            .flatMap(byteBuf -> {
+                                // System.out.println(byteBuf.toString(Charset.forName("UTF-8")));
+                                return resp.sendObject(byteBuf.retain());
+                            });
+                })
+                .host("127.0.0.1")
+                .port(8087)
+                .wiretap(true)
+                .bindNow();
+
+        System.out.println("\n>>> Api Gateway Server Running ...\n");
+
+        disposableServer.onDispose().block();
+    }
+}
