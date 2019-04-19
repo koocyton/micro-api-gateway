bc7780fb04e568ea3166eb4e11d808795c346992
yi.liu <liuyi@doopp.com>
2019-04-19 18:16:32 +0800

    api gateway

--- src/main/java/com/doopp/api/gateway/GatewayEtcdUtil.java
+++ src/main/java/com/doopp/api/gateway/GatewayEtcdUtil.java
@@ -2,29 +2,78 @@ package com.doopp.api.gateway;
 
 import mousio.client.retry.RetryOnce;
 import mousio.etcd4j.EtcdClient;
-import mousio.etcd4j.requests.EtcdKeyGetRequest;
+import mousio.etcd4j.promises.EtcdResponsePromise;
+import mousio.etcd4j.responses.EtcdAuthenticationException;
+import mousio.etcd4j.responses.EtcdException;
+import mousio.etcd4j.responses.EtcdKeysResponse;
 
+import java.io.IOException;
 import java.net.URI;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.TimeoutException;
 
 public class GatewayEtcdUtil implements Runnable {
 
     private EtcdClient client;
 
-    GatewayEtcdUtil() {
-        URI[] uris = new URI[1];
-        uris[0] = URI.create("http://127.0.0.1:2379");
+    private Map<String, String> serviceBaseUrlMap;
+
+    GatewayEtcdUtil(Map<String, String> serviceBaseUrlMap, Properties properties) {
+        this.serviceBaseUrlMap = serviceBaseUrlMap;
+        String[] etcdServerList = properties.getProperty("etcd.server.list").split(",");
+        URI[] uris = new URI[etcdServerList.length];
+        for(int ii=0; ii<etcdServerList.length; ii++) {
+            uris[ii] = URI.create(etcdServerList[ii]);
+        }
         client = new EtcdClient(uris);
-        client.setRetryHandler(new RetryOnce(20)); //retry策略
+        // retry 策略
+        client.setRetryHandler(new RetryOnce(20));
+        this.regist();
+    }
+
+    private void regist() { // 注册节点，放在程序启动的入口
+        try {
+            // 用put方法发布一个节点
+            EtcdResponsePromise<EtcdKeysResponse> p = client.put("key", "value").ttl(60).send();
+            // get 会阻塞，由上面client的retry策略决定阻塞的方式, 用来保证设置完成
+            p.get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void destory() {
+        try {
+            EtcdResponsePromise<EtcdKeysResponse> p = client.delete("key").recursive().send();
+            p.get();
+            client.close();
+        } catch (IOException | EtcdException | EtcdAuthenticationException | TimeoutException e) {
+            // TODO Auto-generated catch block
+            // e.printStackTrace();
+        }
     }
 
     @Override
-    public void run() {
+    public synchronized void run() {
+        try {
+            EtcdResponsePromise<EtcdKeysResponse> etcdResponsePromise = client.getAll().recursive().send();
+            etcdResponsePromise.addListener(promise -> {
+                System.out.println(promise);
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        int ii=0;
         while(true) {
             try {
-                Thread.sleep(40 * 1000L);
-                EtcdKeyGetRequest etcdKeyGetRequest = client.getAll();
-            } catch (InterruptedException e) {
+                Thread.sleep(618);
+                ii ++;
+                client.put("key" + ii, "value" + ii).ttl(60).send();
+                //System.out.println(etcdResponsePromise);
+                //this.serviceBaseUrlMap.put("", "");
+            } catch (InterruptedException | IOException e) {
                 e.printStackTrace();
             }
         }
--- src/main/java/com/doopp/api/gateway/GatewayServer.java
+++ src/main/java/com/doopp/api/gateway/GatewayServer.java
@@ -8,15 +8,48 @@ import reactor.netty.DisposableServer;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.http.server.HttpServer;
 
+import javax.annotation.PreDestroy;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 public class GatewayServer {
 
-    public static void main(String[] args) {
+    private static Map<String, String> serviceBaseUrlMap = new HashMap<>();
 
-        HttpClient httpClient = HttpClient.create();
+    private static GatewayEtcdUtil gatewayEtcdUtil;
+
+    public static void main(String[] args) throws IOException {
+
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(args[0]));
 
         // 抛出线程监听 etcd ，并刷新 Api Gateway 的路由表
-        GatewayEtcdUtil gatewayEtcdUtil = new GatewayEtcdUtil();
+        gatewayEtcdUtil = new GatewayEtcdUtil(serviceBaseUrlMap, properties);
         new Thread(gatewayEtcdUtil).start();
+
+        // 运行 Api Gateway
+        GatewayServer gatewayServer = new GatewayServer();
+        gatewayServer.launch();
+    }
+
+    private static HttpClient httpClient = HttpClient.create();
+
+    /**
+     * 从 uri ( /api/v1/user/1 ) 判断请求的微服务地址
+     *
+     * @param uri request Uri
+     * @return String
+     */
+    private String getServiceBaseUrl(String uri) {
+        String key = uri.replaceAll("^(/[^/]+/[^/]+/[^/]+)/.*$", "$1");
+        return serviceBaseUrlMap.get(key);
+    }
+
+    private void launch() {
 
         // 启动 API Gateway Server
         DisposableServer disposableServer = HttpServer.create()
@@ -25,8 +58,15 @@ public class GatewayServer {
                 )
                 .handle((req, resp) -> {
 
+                    String serviceUri = req.uri();
+                    String serviceBaseUrl = getServiceBaseUrl(serviceUri);
+
+                    if (serviceBaseUrl==null) {
+                        return resp.sendNotFound();
+                    }
+
                     HttpClient httpClientRequest = httpClient
-                            .baseUrl("https://www.doopp.com")
+                            .baseUrl(serviceBaseUrl)
                             .headers(entries -> {
                                 HttpHeaders httpHeaders = req.requestHeaders();
                                 entries.set("User-Agent", httpHeaders.get("User-Agent"));
@@ -40,21 +80,29 @@ public class GatewayServer {
 
                     // POST
                     if (req.method().equals(HttpMethod.POST)) {
-                        responseReceiver = httpClientRequest.post()
+                        responseReceiver = httpClientRequest
+                                .post()
+                                .uri(serviceUri)
                                 .send(req.receiveContent().ofType(ByteBuf.class));
                     }
                     // PUT
                     else if (req.method().equals(HttpMethod.PUT)) {
-                        responseReceiver = httpClientRequest.put()
+                        responseReceiver = httpClientRequest
+                                .put()
+                                .uri(serviceUri)
                                 .send(req.receiveContent().ofType(ByteBuf.class));
                     }
                     // DELETE
                     else if (req.method().equals(HttpMethod.DELETE)) {
-                        responseReceiver = httpClientRequest.delete();
+                        responseReceiver = httpClientRequest
+                                .delete()
+                                .uri(serviceUri);
                     }
                     // GET
                     else {
-                        responseReceiver = httpClientRequest.get().uri("/phpinfo.php");
+                        responseReceiver = httpClientRequest
+                                .get()
+                                .uri(serviceUri);
                     }
 
                     return responseReceiver.response((httpClientResponse, byteBufFlux)->{
@@ -74,5 +122,10 @@ public class GatewayServer {
         System.out.println("\n>>> Api Gateway Server Running ...\n");
 
         disposableServer.onDispose().block();
+    }
+
+    @PreDestroy
+    public void destory() {
+        gatewayEtcdUtil.destory();
     }
 }
